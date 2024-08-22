0c2d69d048fd57edd96e859384fa5936525d5e2a
Thornblaster12345 <alexvinuk@icloud.com>
2024-08-22 11:46:49 +0530

    Small things

--- VariableTest.py
+++ VariableTest.py
@@ -1,12 +1,15 @@
 from SimulationFiles.Simulation import Simulation
 
 if __name__ == "__main__":
+
+    # Modify these variables
     num_simulations = 1
     num_points = 4
     human_weight = 1
     ball_weight = 1
     points = [[1, 2], [3, 5], [6, 1], [8, 3]]
 
+    # Don't touch this code
     simulation = Simulation(num_simulations, num_points, human_weight, ball_weight, points)
     simulation.run_simulation()
     simulation.save_to_csv('var_test.csv')
