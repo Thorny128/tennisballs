68fa02f84333183fc88236981c4d0097dc486333
Thornblaster12345 <alexvinuk@icloud.com>
2024-08-23 19:55:16 +0530

    Small quality of life improvements

--- RandomSim.py
+++ RandomSim.py
@@ -1,8 +1,11 @@
 from SimulationFiles.Simulation import Simulation
 
 if __name__ == "__main__":
+    # Modify this
+    NUM_SIMULATIONS = 10
+
     print("Starting Simulations")
-    simulation = Simulation(10)
+    simulation = Simulation(NUM_SIMULATIONS)
     simulation.run_simulation()
     simulation.save_to_csv('random_sim_output.csv')
     print("Done!")
--- VariableTest.py
+++ VariableTest.py
@@ -1,16 +1,15 @@
 from SimulationFiles.Simulation import Simulation
 
 if __name__ == "__main__":
-    print("Starting Simulation")
-
     # Modify these variables
     num_simulations = 1
     num_points = 4
-    human_weight = 190
-    ball_weight = 87.5
-    points = [[0, 0], [0, 10], [3, 8], [4, 7], [4, 6], [8, 7], [9, 7], [7, 3], [8, 0], [4, 0], [0, 2], [0, 0]]
+    human_weight = 1
+    ball_weight = 1
+    points = [[0, 0], [1, 2], [3, 5], [6, 1], [8, 3], [0, 0]]
 
     # Don't touch this code
+    print("Starting Simulation")
     simulation = Simulation(num_simulations, num_points, human_weight, ball_weight, points)
     simulation.run_simulation()
     simulation.save_to_csv('var_test.csv')
