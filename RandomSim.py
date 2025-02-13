7a64dcc077b2a78a64d8011668e37fcf9dfa71f1
Alex Kuriakose <alexvinuk@icloud.com>
2025-02-12 21:29:29 -0500

    1000 sim run

--- RandomSim.py
+++ RandomSim.py
@@ -2,7 +2,7 @@ from SimulationFiles.Simulation import Simulation
 
 if __name__ == "__main__":
     # Change the value below
-    NUM_SIMULATIONS = 10
+    NUM_SIMULATIONS = 1000
 
     print("Starting Simulations")
     simulation = Simulation(NUM_SIMULATIONS)
